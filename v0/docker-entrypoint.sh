556fcf9f6099d494404241a40f2b428e08bf40ad
honeok <i@honeok.com>
2026-02-18 05:01:44 +0800

    chore: remove unused entrypoint scripts
    Signed-off-by: honeok <i@honeok.com>

--- v0/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2024 The Nezha Authors. All rights reserved.
-
-printf "nameserver 127.0.0.11\nnameserver 8.8.4.4\nnameserver 223.5.5.5\n" >/etc/resolv.conf
-exec /dashboard/app
